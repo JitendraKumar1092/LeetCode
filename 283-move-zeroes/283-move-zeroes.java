ee1805bd29b876c187cc07e5c27d66beddeb9328
Jitendra Kumar <jitendra.kumar_cs20@gla.ac.in>
2022-09-14 23:27:10 +0530

    Time: 522 ms (5.01%), Space: 54.6 MB (66.79%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -1,17 +1,13 @@
 class Solution {
     public void moveZeroes(int[] array) {
         int size = array.length;
-    
-    
     for (int i = 0; i < size - 1; i++)
-    
-    
-      for (int j = 0; j < size - i - 1; j++)
 
-    
-        if (array[j] == 0) {
+        for (int j = 0; j < size - i - 1; j++)
 
-    
+            if (array[j] == 0) {
+
+    // swap karlo guys
           int temp = array[j];
           array[j] = array[j + 1];
           array[j + 1] = temp;
