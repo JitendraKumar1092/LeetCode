5c732d1c33727d8f6dca1f0e528337f39f9a31bb
Jitendra Kumar <jitendra.kumar_cs20@gla.ac.in>
2022-11-16 14:41:23 +0530

    Time: 54 ms (64.59%), Space: 13.9 MB (10.30%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.py
@@ -0,0 +1,10 @@
+class Solution:
+    def detectCapitalUse(self, s: str) -> bool:
+        if s == s.lower():
+            return True
+        if s == s.upper():
+            return True
+        if s == s.title():
+            return True
+        return False
+        
