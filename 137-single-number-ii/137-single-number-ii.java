4f21ac22cf26ce1cefd92dab746a5eb242afc7bb
Jitendra Kumar <jitendra.kumar_cs20@gla.ac.in>
2022-11-21 23:57:23 +0530

    Time: 6 ms (66.35%), Space: 44.4 MB (36.06%) - LeetHub

---
+++ 137-single-number-ii/137-single-number-ii.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ans=0;
+        for(int i=0; i<32; i++){
+            int t=0;
+            for(int j=0; j<nums.length; j++){
+                t+=(nums[j]&1);
+                nums[j]>>=1;
+            }
+            ans+=((t%3)<<i);
+        }
+        return ans;
+    }
+}
