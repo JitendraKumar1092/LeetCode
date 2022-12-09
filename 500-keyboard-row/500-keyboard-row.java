3da4ff9e39c6f371308183ab47e2b46e8549acd4
Jitendra Kumar <jitendra.kumar_cs20@gla.ac.in>
2022-12-09 22:41:17 +0530

    Time: 2 ms (48.08%), Space: 42.8 MB (5.78%) - LeetHub

--- 500-keyboard-row/500-keyboard-row.java
+++ 500-keyboard-row/500-keyboard-row.java
@@ -18,17 +18,17 @@ class Solution {
         List<String> arr2 = new ArrayList();
         for(int i=0; i<words.length; i++){
             String temp = words[i];
-            int count1 = 0;
+            int count = 0;
             int count2 = 0;
             int count3 = 0;
             for(int j=0; j<temp.length(); j++){
-                if(map.get(temp.charAt(j)) != null) count1++;
+                if(map.get(temp.charAt(j)) != null) count++;
                 else if(map2.get(temp.charAt(j)) != null) count2++;
                 else if(map3.get(temp.charAt(j)) != null) count3++;
             }
-            if(count1 != 0 && count2 == 0 && count3 == 0) arr2.add(temp);
-            else if(count1 == 0 && count2 != 0 && count3 == 0) arr2.add(temp);
-            else if(count1 == 0 && count2 == 0 && count3 != 0) arr2.add(temp);
+            if(count != 0 && count2 == 0 && count3 == 0) arr2.add(temp);
+            else if(count == 0 && count2 != 0 && count3 == 0) arr2.add(temp);
+            else if(count == 0 && count2 == 0 && count3 != 0) arr2.add(temp);
         }
         return arr2.toArray(new String[0]);
     }
