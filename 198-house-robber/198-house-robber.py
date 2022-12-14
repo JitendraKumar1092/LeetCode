690ee5b0f83d6d82875a11e8a8f6ef8ad47fd9a1
Jitendra Kumar <jitendra.kumar_cs20@gla.ac.in>
2022-12-14 22:34:45 +0530

    Time: 36 ms (86.46%), Space: 13.9 MB (66.57%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -2,6 +2,6 @@ class Solution:
 
     def rob(self, nums):
         dp1 = dp2 = 0
-        for n in nums: 
-            dp2, dp1 = dp1, max(dp1, dp2 + n)
+        for i in nums: 
+            dp2, dp1 = dp1, max(dp1, dp2 + i)
         return dp1
