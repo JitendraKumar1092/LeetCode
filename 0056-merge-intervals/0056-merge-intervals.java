238bb56e9a5f7f3dca592728d706e2310da6260c
Jitendra Kumar <jitendra.kumar_cs20@gla.ac.in>
2023-11-02 15:40:59 +0530

    Time: 8 ms (91.65%), Space: 45 MB (95.01%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,31 +1,28 @@
 import java.util.*;
 class Solution {
-    public int[][] merge(int[][] intervals) {
+    public int[][] merge(int[][] rrs) {
         
-    if (intervals == null || intervals.length <= 1) {
-        return intervals;
+    if (rrs == null || rrs.length <= 1) {
+        return rrs;
     }
 
-    Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+    Arrays.sort(rrs, (a, b) -> Integer.compare(a[0], b[0]));
 
     List<int[]> ans = new ArrayList<>();
-    int[] current = intervals[0];
-
-    for (int i = 1; i < intervals.length; i++) {
-        int[] next = intervals[i];
-
-        if (current[1] >= next[0]) {
-            
-            current[1] = Math.max(current[1], next[1]);
-        } else {
-            
+    int [] current = rrs[0];
+        for  (int i=1; i<rrs.length;i++){
+            int [] next = rrs[i];
+            if (current[1]>=next[0]){
+                current[1]= Math.max(current[1] , next[1]);
+                
+            }
+            else{
+                ans.add(current);
+                current =next;
+            }
+            }
             ans.add(current);
-            current = next;
-        }
-    }
-
-  
-    ans.add(current);
+        
 
     return ans.toArray(new int[0][]);
 
