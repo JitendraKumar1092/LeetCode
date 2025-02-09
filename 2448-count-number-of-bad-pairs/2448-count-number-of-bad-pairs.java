6056e2cd2ce058953152a33f4b59ae7031648b9f
Jitendra Kumar <jitendra.kumar_cs20@gla.ac.in>
2025-02-09 23:50:46 +0530

    Time: 27 ms (100%), Space: 59.2 MB (34.71%) - LeetHub

--- 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.java
+++ 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.java
@@ -2,6 +2,8 @@ class Solution {
     public long countBadPairs(int[] nums) {
         Map<Integer, Integer> bag = new HashMap<>();
         long count = 0;
+
+        
         for (int i = 0; i < nums.length; i++) {
             count += -1 + bag.merge(nums[i] - i, 1, Integer::sum);
         }
@@ -9,7 +11,7 @@ class Solution {
 
         int len = nums.length;
 
-        
+
         return 1L * len * (len - 1) / 2 - count;
     }
 }
