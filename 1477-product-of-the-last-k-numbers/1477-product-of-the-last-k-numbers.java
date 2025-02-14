ed391c809f85ca4faa753137f856796ca977ffbd
Jitendra Kumar <jitendra.kumar_cs20@gla.ac.in>
2025-02-14 11:18:54 +0530

    Time: 15 ms (91.34%), Space: 68.8 MB (74.65%) - LeetHub

--- 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.java
+++ 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.java
@@ -25,10 +25,3 @@ class ProductOfNumbers {
         
     }
 }
-
-/**
- * Your ProductOfNumbers object will be instantiated and called as such:
- * ProductOfNumbers obj = new ProductOfNumbers();
- * obj.add(num);
-
- */
