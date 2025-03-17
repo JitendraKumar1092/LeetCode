16e9d7f832f6a93980e18c6e06ef89dd3615aa8b
Jitendra Kumar <jitendra.kumar_cs20@gla.ac.in>
2025-03-18 00:27:24 +0530

    Time: 0 ms (100%), Space: 42.5 MB (88.04%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -13,13 +13,14 @@
  *     }
  * }
  */
+
 class Solution {
     public int maxDepth(TreeNode root) {
-         if (root == null ) 
-        return 0;
-        Solution s = new Solution();
-        int leftDepth = s.maxDepth(root.left);
-        int rightDepth = s.maxDepth(root.right);
-        return 1 + Math.max(leftDepth , rightDepth);
+        if ( root == null) return 0;
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return Math.max(left , right) + 1;
+        
+
     }
 }
